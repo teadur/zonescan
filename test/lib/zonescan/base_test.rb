944104ee5efa3b505ccea390b03ee9ed2c343347
georg <georg@gj.ee>
2017-03-09 13:30:27 +0200

    add tests for base

---
+++ test/lib/zonescan/base_test.rb
@@ -0,0 +1,31 @@
+require_relative '../../test_helper'
+ 
+describe Zonescan::Base do
+ 
+  subject { Zonescan::Base }
+ 
+  describe "reading from file" do
+ 
+    it "must have a source" do
+      subject.must_respond_to(:source)
+    end
+ 
+    it "must have the dracula file as a source" do
+      subject.source.must_be_instance_of(String)
+    end
+ 
+  end
+ 
+  describe "splitting into lines" do
+ 
+    it "must correctly split the file into lines" do
+      subject.processed_source.must_be_instance_of(Array)
+    end
+ 
+    it "must correctly remove empty lines" do
+      subject.processed_source.wont_include(nil)
+    end
+ 
+  end
+ 
+end
