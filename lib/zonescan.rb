cc2d342dec53f5371492378feb54d82bb1e3c067
Georg Kahest <georg@tld.ee>
2017-03-14 15:03:07 +0200

    robocop fixes

--- lib/zonescan.rb
+++ lib/zonescan.rb
@@ -7,29 +7,28 @@ require 'resolv'
 # Zonescan::
 module Zonescan
   # Your code goes here...
-  Failed = Array.new
-  Completed = Array.new
-  Untested = Array.new
+  Failed = Array[]
+  Completed = Array[]
+  Untested = Array[]
 
   puts __FILE__
   # name = Domain.domain
   # puts name
   execute = Domain.domains_all
-  total = execute.count()
+  total = execute.count
   Untested.each do |name|
-  if Domain.validate(name) == true
-    puts "lets call out scanner for that name #{name}"
-    rvalue =  Httpscan.check(name)
-    # add domains with return code 200-500 to Completed list
-    # TODO: Figure out exact return codes i care about
-    if rvalue >= 200 && rvalue <= 500
-      Completed.push(name)
-    end
+    if Domain.validate(name) == true
+      puts "lets call out scanner for that name #{name}"
+      rvalue =  Httpscan.check(name)
+      # add domains with return code 200-500 to Completed list
+      # TODO: Figure out exact return codes i care about
+      Completed.push(name) if rvalue >= 200 && rvalue <= 500
   else
     Failed.push(name)
-    puts "lets remove that name from uncompleted list / add it to failed list #{name}"
-  end
+    puts "remove name from uncompleted list / add it to failed list #{name}"
+    end
 
   end
-  puts "Total: #{total}\nFailed (#{Failed.count}): #{Failed} \nCompleted(#{Completed.count}): #{Completed}"
+  puts "Total: #{total}\nFailed (#{Failed.count}): #{Failed} \n"
+  puts "Completed(#{Completed.count}): #{Completed}"
 end
