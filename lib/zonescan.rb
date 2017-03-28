99e01375a080653a8a895749d75ab53fc7fad5c0
Georg Kahest <georg@tld.ee>
2017-03-28 14:46:32 +0300

    Provide help for cli, start moving to cli implementations

--- lib/zonescan.rb
+++ lib/zonescan.rb
@@ -2,6 +2,7 @@ require_relative 'zonescan/version'
 require_relative 'zonescan/base'
 require_relative 'zonescan/domain'
 require_relative 'zonescan/http_scan'
+require_relative 'zonescan/cli'
 require 'curb'
 require 'resolv'
 # Zonescan::
---
+++ lib/zonescan/cli.rb
@@ -0,0 +1,37 @@
+# Zonescan Module
+module Zonescan
+  # Zonescan::Cli
+  class Cli
+    def self.run
+      require 'optparse'
+
+      ARGV << '-h' if ARGV.empty?
+      options = {}
+      ARGV.options do |opts|
+        opts.banner = "Usage: #{PROJECT} [file or files] \n"\
+                      "       #{PROJECT} [options]"
+        opts.on('-f', '--files [files]',           Array,     'Comma separated files to check')     { |val| options['files'] = val }
+        opts.on('-r', '--record',                    TrueClass, 'Record results')         { |val| options['record'] = val }
+        opts.on('-s', '--show [names]',           Array,     'Comma separated list of names to show')     { |val| options['show'] = val }
+
+        opts.on_tail('--help') do
+          puts opts
+          exit
+        end
+        opts.parse!
+      end
+
+# Figure out what files to parse
+      files = options['files']
+      if files.nil?
+        files = []
+        ARGV.each do |a|
+          files.push a if a !~ /^--.*/
+        end
+      end
+
+
+      puts "cli #{files}"
+    end
+  end
+end
